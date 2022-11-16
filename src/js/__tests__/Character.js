c8b56fc8118323a790908f28d4ed50a53b67b82d
Marko Melle <markmelle1@gmail.com>
2022-11-16 17:22:15 +0300

    add test v2

--- src/js/__tests__/Character.js
+++ src/js/__tests__/Character.js
@@ -1,7 +1,18 @@
 import Character from '../Character';
 
-test('some test', () => {
+test.each([
+  ['1'],
+  ['12345678910'],
+])(
+  ('Test name length Error'), (name) => {
+    const received = () => new Character(name);
+    expect(received).toThrow(Error('Имя должено быть не менее 2 и не более 10 символов'));
+  },
+);
+
+test('Test stats of Character', () => {
   const received = new Character('John');
   const expected = { name: 'John', health: 100, level: 1 };
+
   expect(received).toEqual(expected);
 });
